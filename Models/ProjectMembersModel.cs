edb61f4089af1cce5c1a6665f1c3728a84fa0aa0
Neferu Ana-Maria <48251558+marianeferu@users.noreply.github.com>
2020-01-18 15:49:13 +0200

    Add files via upload

---
+++ Models/ProjectMembersModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iBusinessManagement.Models
+{
+    public class ProjectMembersModel
+    {
+        [Display(Name = "User")]
+        public int ProjectId { get; set; }
+
+        public string SelectedUserId { get; set; }
+
+        public IEnumerable<SelectListItem> UsersSelect { get; set; }
+    }
+}
